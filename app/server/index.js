35abbbfc82fc5dffacc02ec07c6cbe113dd6ba1c
Nazira <nazira@wantokjobs.com>
2026-02-16 14:40:33 +0800

    fix: add fs require to index.js

--- app/server/index.js
+++ app/server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
